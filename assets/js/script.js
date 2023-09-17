0b1a6d01ba06dc93e0b66be9896e5c876a820f5b
Rijo Sebastian <rijobdk@gmail.com>
2023-09-17 17:42:13 +0530

    Finished the main section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 let dashboardIcon = document.querySelector(".large-icon");
 let title = document.querySelectorAll(".title");
+let starIcon = document.querySelectorAll(".star");
 
 dashboardIcon.addEventListener("click", () => {
   title.forEach((item) => {
     item.classList.toggle("hidden");
+  });
+});
+
+starIcon.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
   });
 });
